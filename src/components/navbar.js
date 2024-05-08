889914cc599d9547b603c8e32d96a24c2e9cb671
Rémi <115152594+remimoul@users.noreply.github.com>
2024-05-08 14:51:08 +0200

    Remove unnecessary test files and dependencies

---
+++ src/components/navbar.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function Navbar() {
+  return (
+    <nav>
+      <ul>
+        <li><a href="/">Agir</a></li>
+        <li><a href="/">Evenements</a></li>
+        <li><a href="/contact">Article</a></li>
+        <li><a href="/about">Témoignage</a></li>
+        <li>S'orienter</li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Navbar;
