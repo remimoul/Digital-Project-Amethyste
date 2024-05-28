147e9c8656c0b44eae872bc04bd8f959527f1ba3
Rémi <115152594+remimoul@users.noreply.github.com>
2024-05-29 01:14:42 +0200

    maj

--- src/components/about.js
+++ src/components/about.js
@@ -7,6 +7,21 @@ function About(){
         <>
             <Navbar />
             <h1>A Propos</h1>
+            <p>
+
+GRINEASY C’EST QUOI ? 
+Une application incarnant un Happiness Officer à part entière, dédiée à la santé mentale en milieu professionnel. 
+Nous offrons aux entreprises un outil complet pour veiller au bien-être de leurs employés et au maintien à long terme de celui-ci. 
+Notre promesse ? Une réduction du turnover et de l’absentéisme. Une augmentation de la productivité et donc du chiffre d’affaire. 
+
++ INSÉRER UNE PHOTO DE CHACUN 
+
+*Photo d’Erika* Erika, e-commerce
+*Photo de Céline* Céline, création numérique 
+*Photo de Kenza* Kenza, Chef de projet & webmarketing 
+*Photo de Rémi* Rémi, développeur 
+*Photo de Aymane* Aymane, digital 
+            </p>
             <Footer />
         </>
     )
